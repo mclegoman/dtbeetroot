315f2a514c6969ebe6a79a5f0afc9de4e4f55f2f
dannytaylor <daniel@mclegoman.com>
2025-04-04 16:28:07 +1300

    fabric api

--- src/main/java/com/mclegoman/dtbeetroot/BeetrootEdition.java
+++ src/main/java/com/mclegoman/dtbeetroot/BeetrootEdition.java
@@ -7,11 +7,17 @@
 
 package com.mclegoman.dtbeetroot;
 
+import com.mclegoman.dtbeetroot.items.ItemRegistry;
+import com.mclegoman.dtbeetroot.mine_effects.MineEffectRegistry;
+import com.mclegoman.dtbeetroot.player_unlocks.PlayerUnlockRegistry;
 import net.fabricmc.api.ModInitializer;
 
 public class BeetrootEdition implements ModInitializer {
 	public static final String modId = "dtbeetroot";
 	@Override
 	public void onInitialize() {
+		ItemRegistry.init();
+		PlayerUnlockRegistry.init();
+		MineEffectRegistry.init();
 	}
 }
